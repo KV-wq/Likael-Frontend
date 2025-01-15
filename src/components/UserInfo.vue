05751f8c4091442e6f92efb48e6c57011518c388
konstantin <refundsix@mail.ru>
2025-01-15 15:34:39 +0700

    auth

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,10 +1,14 @@
-<script setup></script>
+<script setup>
+import { useUserStore } from "../stores/user";
+
+const user = useUserStore().userData;
+</script>
 
 <template>
   <div class="d-flex flex-column align-items-center">
-    <img src="../assets/avatar.jpg" class="avatar" />
-    <p class="text-white fs-3 mb-0 mt-2">Иван Иванов</p>
-    <p class="text-white fs-6 fw-light opacity-50">IvanIvanov</p>
+    <img :src="user.photo_url" class="avatar" />
+    <p class="text-white fs-3 mb-0 mt-2">{{ user.fullname }}</p>
+    <p class="text-white fs-6 fw-light opacity-50">{{ user.username }}</p>
   </div>
 </template>
 
