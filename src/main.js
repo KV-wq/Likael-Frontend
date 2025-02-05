b21a75c19ebe8af7095392c51563e10c426b94b5
konstantin <refundsix@mail.ru>
2025-02-06 00:29:47 +0700

    naeb

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import router from "./router/router";
 import { createPinia } from "pinia";
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://likael-backend.onrender.com";
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "https://likael-backend.onrender.com";
+// axios.defaults.baseURL = "http://localhost:5000";
 
 const pinia = createPinia();
 
